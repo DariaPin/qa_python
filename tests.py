fb6d0072594aaf7fe81af99804334a813af2deda
Daria Pinchuk <dariaviaconnect@gmail.com>
2025-01-17 21:39:40 +0300

    new commit На ревью исправленный

--- tests.py
+++ tests.py
@@ -121,4 +121,3 @@ class TestBooksCollector:
         collector.favorites = ["Book1", "Book2"]
         result = collector.get_list_of_favorites_books()
         assert result == ["Book1", "Book2"]
-        print("Book1")
